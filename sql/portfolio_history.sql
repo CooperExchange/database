75f57cda835591a791ee7791c26cf3208a2cf8a3
Sangjoon (Bob) Lee <bobleesj@gmail.com>
2023-03-01 08:02:19 -0500

    Update portfolio_history.sql

--- sql/portfolio_history.sql
+++ sql/portfolio_history.sql
@@ -2,5 +2,5 @@ CREATE TABLE portfolio_history (
     portfolio-history_id SERIAL PRIMARY KEY,
     user_id      bigint           NOT NULL,
     portfolio_value NUMERIC(1000,2) NOT NULL,
-    date_balance DATE DEFAULT CURRENT_DATE,
+    date_balance DATE DEFAULT CURRENT_DATE
 );
