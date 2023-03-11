bdc76b40fb13439edff62d740ce9743d9e5e46b1
Sangjoon (Bob) Lee <bobleesj@gmail.com>
2023-03-10 20:03:54 -0500

    Display timestamp

--- sql/portfolio_history.sql
+++ sql/portfolio_history.sql
@@ -2,5 +2,5 @@ CREATE TABLE portfolio_history (
     portfolio_history_id SERIAL PRIMARY KEY,
     user_id      bigint           NOT NULL,
     portfolio_value NUMERIC(1000,2) NOT NULL,
-    date_balance DATE DEFAULT CURRENT_DATE
+    date_balance TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
